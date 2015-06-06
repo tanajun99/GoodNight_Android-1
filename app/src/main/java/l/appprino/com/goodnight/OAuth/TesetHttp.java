5b5cf89f8df4a15b7ae7bbd31040a1ad1137392f
白川義丸 <rannocat@yahoo.co.jp>
2015-06-07 04:06:57 +0900

    TestHttp.java を作成。http通信にはDefaultHttpClientを使うことにしました

--- app/src/main/java/l/appprino/com/goodnight/OAuth/TesetHttp.java
+++ app/src/main/java/l/appprino/com/goodnight/OAuth/TesetHttp.java
@@ -4,11 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
+import org.json.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * Created by shirakawayoshimaru on 15/06/07.
@@ -18,6 +32,7 @@ public class TesetHttp extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("HTML", "START");
         Test();
 
     }
@@ -27,17 +42,34 @@ public class TesetHttp extends Activity {
         new Thread(new Runnable() {
             @Override
             public void run() {
+                HttpClient client = new DefaultHttpClient();
+
+                // パラメータの設定
+                ArrayList<NameValuePair> value = new ArrayList<NameValuePair>();
+                value.add(new BasicNameValuePair("server_token", "u1_Czj8E0L5Py6FUNVH1OZShbO1FC26Hxitk_8zc"));
+                value.add(new BasicNameValuePair("latitude", "37.775818"));
+                value.add(new BasicNameValuePair("longitude", "-122.418028"));
+
+                String responseData = null;
+                String URL ="https://api.uber.com/v1/products";
                 try {
-                    HttpClient httpClient = new DefaultHttpClient();
-                    HttpGet httpGet = new HttpGet("https://www.google.com/");
-                    HttpResponse httpResponse = httpClient.execute(httpGet);
-                    String str = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
-                    Log.d("HTTP", str);
-                } catch (Exception ex) {
-                    System.out.println(ex);
+                    String query = URLEncodedUtils.format(value, "UTF-8");
+                    HttpGet get = new HttpGet(URL + "?" + query);
+
+                    // リクエスト送信
+                    HttpResponse response = client.execute(get);
+                    // 取得
+                    HttpEntity entity = response.getEntity();
+                    responseData = EntityUtils.toString(entity, "UTF-8");
+                    Log.d("HTML",responseData);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         }).start();
 
     }
+
+
+
 }
