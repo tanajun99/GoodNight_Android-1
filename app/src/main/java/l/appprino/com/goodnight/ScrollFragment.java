8653a928b7f0ba589b488cc97d6c3e1de6f7c907
tanajun.99@gmail.com <tanajun.99@gmail.com>
2015-06-07 08:57:47 +0900

    Modify

--- app/src/main/java/l/appprino/com/goodnight/ScrollFragment.java
+++ app/src/main/java/l/appprino/com/goodnight/ScrollFragment.java
@@ -10,20 +10,22 @@ import android.view.ViewGroup;
 import com.github.florent37.materialviewpager.MaterialViewPagerHelper;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 
 public class ScrollFragment extends Fragment {
 
     private ObservableScrollView mScrollView;
-    private Fragment mMapFragment;
+    MapView mMapFragment;
+    GoogleMap mMap;
 
-    public static ScrollFragment newInstance() {
+    public static Fragment newInstance() {
         return new ScrollFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_scroll, container, false);
-
     }
 
     @Override
@@ -31,8 +33,31 @@ public class ScrollFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         mScrollView = (ObservableScrollView) view.findViewById(R.id.scrollView);
-        mMapFragment= getFragmentManager().findFragmentById(R.id.map);
+
+        mMapFragment = (MapView)view.findViewById(R.id.map);
+        mMapFragment.onCreate(savedInstanceState);
+
+        mMap = mMapFragment.getMap();
+        mMap.setMyLocationEnabled(true);
 
         MaterialViewPagerHelper.registerScrollView(getActivity(), mScrollView, null);
+    }
+
+    @Override
+    public void onResume() {
+        mMapFragment.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapFragment.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapFragment.onLowMemory();
     }
 }
