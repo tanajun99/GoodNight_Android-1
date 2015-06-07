27ff472864676e3eba9dae7e48757ade52af04a6
白川義丸 <rannocat@yahoo.co.jp>
2015-06-07 16:05:47 +0900

    make register button click event

--- app/src/main/java/l/appprino/com/goodnight/DetailActivity.java
+++ app/src/main/java/l/appprino/com/goodnight/DetailActivity.java
@@ -2,6 +2,11 @@ package l.appprino.com.goodnight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import l.appprino.com.goodnight.OAuth.OAuthClient;
 
 
 public class DetailActivity extends AppCompatActivity {
@@ -10,5 +15,21 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        Button register =(Button) findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Register();
+            }
+        });
+
+    }
+
+    void Register() {
+        try {
+            OAuthClient.RequestUber(this);
+        } catch (Exception e) {
+            Log.d("Detail:",e.toString());
+        }
     }
 }
